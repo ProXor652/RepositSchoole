551355b9c0b5cf50b934f5a5dbd5c10b607c172c
ProXor652 <apmetalnikov@gmail.com>
2022-08-21 21:26:33 +0700

    Task 1,2,3

--- HomeWork 1/task 1.cs
+++ HomeWork 1/task 1.cs
@@ -1,17 +1,64 @@
-﻿Console.WriteLine("Введите первое число ");
+﻿//Задача 1
+
+//Console.WriteLine("Введите первое число ");
+//string astr = Console.ReadLine();
+//int a = int.Parse(astr);
+
+//Console.WriteLine("Введите второе число ");
+//string bstr = Console.ReadLine();
+//int b = int.Parse(bstr);
+
+//int max = a;
+
+//if(a>b);
+
+//else
+//{
+  //  max=b;
+//}
+//Console.WriteLine(max);
+
+//Задача 2
+
+//Console.WriteLine("Введите первое число ");
+//string astr = Console.ReadLine();
+//int a = int.Parse(astr);
+
+//Console.WriteLine("Введите второе число ");
+//string bstr = Console.ReadLine();
+//int b = int.Parse(bstr);
+
+//Console.WriteLine("Введите третие число ");
+//string cstr = Console.ReadLine();
+//int c = int.Parse(cstr);
+
+//int max = a;
+
+//if(a>b)
+//  {
+  //  if(a>c);
+  //}
+    //else if(b>c)
+      // {
+        // max = b;
+       //}
+     //else
+       //  {
+         // max=c;
+        // }
+//Console.WriteLine(max);
+
+//Задача 3
+
+Console.WriteLine("Введите число ");
 string astr = Console.ReadLine();
 int a = int.Parse(astr);
 
-Console.WriteLine("Введите второе число ");
-string bstr = Console.ReadLine();
-int b = int.Parse(bstr);
-
-int max = a;
-
-if(a>b);
-
-else
-{
-    max=b;
-}
-Console.WriteLine(max);
+if(a%2 == 0)
+   {
+    Console.WriteLine("Данное число является чётным");
+   }
+    else
+    {
+    Console.WriteLine("Данное число является нечётным"); 
+    }
