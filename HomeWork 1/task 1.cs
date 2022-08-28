ad4450716ca4007dd86f0c47f533b586aac79503
ProXor652 <apmetalnikov@gmail.com>
2022-08-28 13:44:38 +0700

    proba

--- HomeWork 1/task 1.cs
+++ HomeWork 1/task 1.cs
@@ -1,12 +1,6 @@
 ﻿//Задача 1
 
-//Console.WriteLine("Введите первое число ");
-//string astr = Console.ReadLine();
-//int a = int.Parse(astr);
 
-//Console.WriteLine("Введите второе число ");
-//string bstr = Console.ReadLine();
-//int b = int.Parse(bstr);
 
 //int max = a;
 
